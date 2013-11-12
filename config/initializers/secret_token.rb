6ff175ecfac1f8bb48e5c83296d67ce0a0c93df2
BilgeGiray <gry33b@gmail.com>
2013-11-12 17:15:23 +0100

    Fertiggit add .git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uebung5::Application.config.secret_key_base = '768a440a2db914ef1d01df960f886263272cf782cee4c7086ecee90c34e2b0e5a988b1d6fb9925e198e7e3c0386560da4bdf5783f2c0c7933dbca6bff91863a9'
